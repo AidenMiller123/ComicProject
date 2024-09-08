ab0ecf4c194a7e6251c6b4d8cfec31c372eba4f3
AidenMiller123 <123018143+AidenMiller123@users.noreply.github.com>
2024-09-08 15:26:05 -0400

    GitHub set up

--- ComicMarvelProject/obj/Debug/net8.0/ComicMarvelProject.AssemblyInfo.cs
+++ ComicMarvelProject/obj/Debug/net8.0/ComicMarvelProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ComicMarvelProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77890683ea973683fa88cbeb2ce85fa232c4f9b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ComicMarvelProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ComicMarvelProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
